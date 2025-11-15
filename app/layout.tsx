81e684bdb4bbf9a055ae4898f0f4725642d3b11d
oscar2697 <lindooscar635@gmail.com>
2025-11-14 20:39:41 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+// @ts-ignore: CSS module declaration missing — add "declare module '*.css';" in a .d.ts file to remove this
+import './globals.css';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Stocks",
+  description: "A stock market tracking in real-time stock prices, get latest news and updates on your favorite stocks.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
